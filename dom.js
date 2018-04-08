d8a2a3ba0d31cad838738e3746129e19ec4987ca
iamsourabhh <iamsourabhh@gmail.com>
2018-04-08 19:59:36 +0530

    sp - -implemented basic timer

--- dom.js
+++ dom.js
@@ -3,55 +3,49 @@ var timer;
 
 
 function onStartClick() {
-    
+
     var startButton = document.getElementById("start");
 
     var timerText = document.getElementById("timerText");
-    
-    timer = new Timer(timerText, 1000);
+
+    var totalTime = document.getElementById("totalTime").value;
+
+    if(timer !== undefined) {
+        timer.stopTimer();
+    }
+    timer = new Timer(timerText, totalTime);
 
     timer.startTimer();
-    
+
 }
 
 function onStopClick() {
 
     if (timer === undefined) {
-        
+
         showWarningMessage('First start the time, you asshole!!')
-        
+
         return;
-    
+
     }
-    var stopButton = document.getElementById("stop");
-
-    var timerText = document.getElementById("timerText");
-
-    stopButton.onclick = timer.stopTimer();
+    timer.stopTimer();
 
 }
 
 function onResetClick() {
 
     if (timer === undefined) {
-        
         showWarningMessage('First start the time, you asshole!!')
-        
         return;
-    
     }
-
-    var resetButton = document.getElementById("reset");
-
-    var timerText = document.getElementById("timerText");
-
-    resetButton.onclick = timer.resetTimer();
+    timer.stopTimer()
+    timer.resetTimer();
 
 }
 
-function showWarningMessage(warning){
-    
+function showWarningMessage(warning) {
+
     var timerText = document.getElementById("timerText");
-    
+
     timerText.innerHTML = warning;
 }
--- timer.js
+++ timer.js
@@ -4,18 +4,53 @@
 function Timer(timerText, totalTime) {
     this.timerText = timerText;
     this.totalTime = totalTime;
+    this.ticker = undefined;
 }
 
 Timer.prototype.startTimer = function () {
-    this.changeTimerText('timer started');
+
+    var totalTime = this.totalTime;
+
+    // save setInterval id in ticker, so we can later stop it.
+    this.ticker = setInterval(this.tickerAction.bind(this), 1000);
 }
 
 Timer.prototype.stopTimer = function () {
-    console.log('timer stopped');
+
+    if (this.ticker === undefined) {
+        return false;
+    }
+    // get the id of the running setinterval and stop it.
+
+    clearInterval(this.ticker);
+    this.ticker = undefined;
+
+    return true;
 }
 
 Timer.prototype.resetTimer = function () {
-    console.log('reset timer');
+    this.changeTimerText('Timer Reseted');
+}
+
+Timer.prototype.tickerAction = function () {
+    this.totalTime--;
+    this.changeTimerText(this.formatSeconds(this.totalTime));
+    if (this.totalTime === 0) {
+        clearInterval(this.ticker);
+    }
+}
+
+Timer.prototype.formatSeconds = function (seconds) {
+
+    var hours = Math.floor(seconds / (60 * 60));
+
+    var minutes = Math.floor((seconds / 60)%60);
+
+    var seconds = seconds % 60;
+
+    return (hours < 10 ? '0' + hours : hours) + ":" 
+    + (minutes < 10 ? '0' + minutes : minutes) 
+    + ":" + (seconds<10 ? '0' + seconds : seconds);
 }
 
 Timer.prototype.changeTimerText = function (text) {
